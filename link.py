079e79e88a445cfb399cd23341f6f797c6d653c8
safarova_s <safarova_s@itstep.academy>
2023-04-26 20:43:52 +0400

    commit 2

--- link.py
+++ link.py
@@ -1,6 +1,6 @@
-https://pythonguides.com/python-tkinter-table-tutorial/
-https://lk.rentinhand.ru/ru/registration
-https://www.google.com/search?q=hex+color&oq=hex+color&aqs=chrome..69i57j0i67i650j0i512l8.2635j0j7&sourceid=chrome&ie=UTF-8
+# https://pythonguides.com/python-tkinter-table-tutorial/
+# https://lk.rentinhand.ru/ru/registration
+# https://www.google.com/search?q=hex+color&oq=hex+color&aqs=chrome..69i57j0i67i650j0i512l8.2635j0j7&sourceid=chrome&ie=UTF-8
 
 
 
